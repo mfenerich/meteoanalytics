eea232a67ae1746812bd656eb872d71f539ecac6
mfenerich <marcelfenerich1@gmail.com>
2024-12-02 17:25:31 +0100

    styles

--- app/main.py
+++ app/main.py
@@ -2,26 +2,26 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 from app.core.logging_config import logger
-
 from app.api.v1.aemet import router as aemet_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """Lifespan context for FastAPI to handle startup and shutdown events."""
-    # Startup logic
     logger.info("Starting FastAPI Meteo Service")
     yield
-    # Shutdown logic
     logger.info("Shutting down FastAPI Meteo Service")
 
 app = FastAPI(
     title="Meteo API",
     description="""
-    Add here some nice documentation
+    API providing access to weather data for Antarctica.
 
-    And features
+    Features:
+    - Retrieve temperature, pressure, and wind speed data.
+    - Supports time aggregation (hourly, daily, monthly).
+    - Handles daylight saving adjustments (CET/CEST).
     """,
-    version="0.0.1",
+    version="0.1.0",
     terms_of_service="http://example.com/terms/",
     contact={
         "name": "API Support",
@@ -32,57 +32,38 @@ app = FastAPI(
         "name": "MIT License",
         "url": "https://opensource.org/licenses/MIT",
     },
-    lifespan=lifespan,  # Register the lifespan context
+    lifespan=lifespan,
 )
 
 @app.get("/health", tags=["Health"])
 async def health_check():
     """
     Check the health of the service.
-
     Returns:
         JSONResponse: Status and message indicating health.
     """
+
     return JSONResponse(
         content={"status": "ok", "message": "Service is healthy"}, status_code=200
     )
 
-# Include Temperature Router
+# Include AEMET Router
 app.include_router(
-    aemet_router, prefix="/v1/antartida", tags=["Temperature Management"]
+    aemet_router, prefix="/v1/antartida", tags=["AEMET Antarctica Data"]
 )
 
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request: Request, exc: HTTPException):
-    """
-    Handle HTTP exceptions by logging and returning a standardized response.
-
-    Args:
-        request (Request): The incoming request object.
-        exc (HTTPException): The exception being handled.
-
-    Returns:
-        JSONResponse: A JSON response with the error details.
-    """
+    """Handle HTTP exceptions by logging and returning a standardized response."""
     logger.error(f"HTTP Exception: {exc.detail} - Path: {request.url}")
     return JSONResponse(
         status_code=exc.status_code,
         content={"error": exc.detail, "code": exc.status_code},
     )
 
-
 @app.exception_handler(Exception)
 async def generic_exception_handler(request: Request, exc: Exception):
-    """
-    Handle unexpected exceptions by logging and returning a generic response.
-
-    Args:
-        request (Request): The incoming request object.
-        exc (Exception): The exception being handled.
-
-    Returns:
-        JSONResponse: A JSON response indicating an internal server error.
-    """
+    """Handle unexpected exceptions by logging and returning a generic response."""
     logger.error(f"Unexpected error: {exc} - Path: {request.url}")
     return JSONResponse(
         status_code=500,
