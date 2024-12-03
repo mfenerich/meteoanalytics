4a20585f9671c1d638b82f0309f6a1a8c03175f7
mfenerich <marcelfenerich1@gmail.com>
2024-12-03 20:34:16 +0100

    add create_at to the model

--- app/db/models.py
+++ app/db/models.py
@@ -4,7 +4,7 @@ Database models for the FastAPI application.
 This module defines the SQLAlchemy ORM models used for database tables.
 """
 
-from sqlalchemy import JSON, TIMESTAMP, Column, Index, Integer, String
+from sqlalchemy import JSON, TIMESTAMP, Column, Index, Integer, String, func
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -21,6 +21,7 @@ class WeatherData(Base):
     identificacion = Column(String, nullable=False, index=True)
     fhora = Column(TIMESTAMP, nullable=False, index=True)
     data = Column(JSON, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now(), index=True)
 
     __table_args__ = (
         Index("idx_identificacion_fhora", "identificacion", "fhora"),
