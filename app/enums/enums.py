82bbe287a56c558af0e63b5a4c00e9e22d234dfd
mfenerich <marcelfenerich1@gmail.com>
2024-12-03 12:00:43 +0100

    fix enum pre-commit

--- app/enums/enums.py
+++ app/enums/enums.py
@@ -1,7 +1,25 @@
+"""
+This module defines enumerations for use in the application.
+
+Such as station identifiers, time aggregation levels,
+and weather data types.
+"""
+
 from enum import Enum
 
 
 class Station(str, Enum):
+    """
+    Enumeration of weather stations in Antarctica.
+
+    Attributes:
+        JUAN_CARLOS_I: Meteorological station "Juan Carlos I."
+        JUAN_CARLOS_I_RADIO: Radiometric station "Juan Carlos I."
+        JUAN_CARLOS_I_RADIO_OLD: Old radiometric station
+            "Juan Carlos I" (until 08/03/2007).
+        GABRIEL_DE_CASTILLA: Meteorological station "Gabriel de Castilla."
+    """
+
     JUAN_CARLOS_I = "89064"  # Estación Meteorológica Juan Carlos I
     JUAN_CARLOS_I_RADIO = "89064R"  # Estación Radiométrica Juan Carlos I
     JUAN_CARLOS_I_RADIO_OLD = (
@@ -11,6 +29,16 @@ class Station(str, Enum):
 
 
 class TimeAggregation(str, Enum):
+    """
+    Enumeration of time aggregation levels for meteorological data.
+
+    Attributes:
+        NONE: No aggregation applied.
+        HOURLY: Data aggregated hourly.
+        DAILY: Data aggregated daily.
+        MONTHLY: Data aggregated monthly.
+    """
+
     NONE = "None"
     HOURLY = "Hourly"
     DAILY = "Daily"
@@ -18,6 +46,15 @@ class TimeAggregation(str, Enum):
 
 
 class DataType(str, Enum):
+    """
+    Enumeration of meteorological data types.
+
+    Attributes:
+        TEMPERATURE: Temperature data in Celsius.
+        PRESSURE: Atmospheric pressure data in hPa.
+        SPEED: Wind speed data in m/s.
+    """
+
     TEMPERATURE = "temperature"
     PRESSURE = "pressure"
     SPEED = "speed"
